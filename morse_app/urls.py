9464d785ce4ee3785588fdcb5802844f53cec9a1
Melissendra <melissendra77@gmail.com>
2021-02-26 14:56:19 +0100

    simplification of authentication's path

--- morse_app/urls.py
+++ morse_app/urls.py
@@ -20,6 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/user/', include('users.urls')),
-    path('api/auth/', include('dj_rest_auth.urls')),
-    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-
+from dj_rest_auth.views import LoginView
+from dj_rest_auth.registration.views import RegisterView
 from .views import UserList, UserDetail
 
 app_name = 'users'
 
 urlpatterns = [
     path('users/', UserList.as_view(), name='user_list'),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail')
+    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='sign_up')
 ]
